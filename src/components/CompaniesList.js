3b8e3e7143d3a20d36bfc56084dcd926fc7d941b
Grandi0z <josephlkhausi@gmail.com>
2023-08-23 16:32:36 +0200

    Add search bar

--- src/components/CompaniesList.js
+++ src/components/CompaniesList.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
 import { NavLink, Link } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
 import PropTypes from 'prop-types';
@@ -23,6 +25,13 @@ const CompaniesList = (props) => {
   return (
     <Container style={{ padding: 0 }}>
       <h2 className="bigTitle">COMPANIES BY NAME</h2>
+      <InputGroup size="sm" className="mb-3">
+        <InputGroup.Text id="inputGroup-sizing-sm">Search</InputGroup.Text>
+        <Form.Control
+          aria-label="Small"
+          aria-describedby="inputGroup-sizing-sm"
+        />
+      </InputGroup>
       <Link className="source" to="https://financialmodelingprep.com/developer/docs/">
         Data provided by Financial Modeling Prep
       </Link>
