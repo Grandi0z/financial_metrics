b0bc4c1610d670b0303c33f84e59250d66bfb088
Grandi0z <josephlkhausi@gmail.com>
2023-08-25 18:49:19 +0200

    Add search bar

--- src/components/FilterCompanyProfiles.js
+++ src/components/FilterCompanyProfiles.js
@@ -7,15 +7,19 @@ import CompaniesList from './CompaniesList';
 import { randomArray } from '../utils/filtersFunctions';
 
 const FilterCompanyProfiles = () => {
+  // set the number of companies which will be display which is 43 - count
+  const count = 37;
+
   const {
     isLoading, error, fetchingProfile,
   } = useSelector((store) => store.companies);
+
   const companiesSymbols = useSelector(arrCompaniesSymbols);
   const dispatch = useDispatch();
   const companies = useSelector(arrCompaniesProfiles);
   // control the useEffect
   let run = true;
-  if (companies.length >= companiesSymbols.length) {
+  if (companies.length >= (companiesSymbols.length - count)) {
     run = false;
   }
   // sometimes the Api sends empty company, so to avoid to use them, let's do a filter
@@ -27,9 +31,10 @@ const FilterCompanyProfiles = () => {
       }
     });
   }
+
   useEffect(() => {
     if (run) {
-      const symbols = randomArray(companiesSymbols, companiesSymbols.length - 40);
+      const symbols = randomArray(companiesSymbols, companiesSymbols.length - count);
       symbols.map((symbol) => dispatch(fetchCompaniesProfiles(symbol)));
     }
     return () => {
@@ -43,7 +48,9 @@ const FilterCompanyProfiles = () => {
   } else if (error) {
     content = <div className="errorMessage">Something went wrong</div>;
     // the condition should be !fetchingProfile && trueCompanies.length >= companiesSymbols.length
-  } else if (!fetchingProfile && trueCompanies.length && companiesSymbols.length) {
+  } else if (!fetchingProfile
+      && (trueCompanies.length >= companiesSymbols.length - count)
+      && companiesSymbols.length) {
     content = <CompaniesList companies={trueCompanies} />;
   } else {
     content = <div>Data are fetching...</div>;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import PropTypes from 'prop-types';
+
+const SearchBar = (props) => {
+  const { companies, setFilterCompanies } = props;
+  const [value, setValue] = useState([]);
+  const handleChange = (value) => {
+    setValue(value);
+    const result = companies.filter((company) => (
+      value
+                && company.companyName.toLowerCase().includes(value)
+    ));
+    setFilterCompanies(result);
+  };
+  return (
+    <InputGroup size="sm" className="mb-3">
+      <InputGroup.Text id="inputGroup-sizing-sm"><i className="bi bi-search" /></InputGroup.Text>
+      <Form.Control
+        aria-label="Small"
+        aria-describedby="inputGroup-sizing-sm"
+        value={value}
+        onChange={(e) => handleChange(e.target.value)}
+      />
+    </InputGroup>
+  );
+};
+
+SearchBar.propTypes = {
+  companies: PropTypes.shape().isRequired,
+  setFilterCompanies: PropTypes.func.isRequired,
+};
+
+export default SearchBar;
